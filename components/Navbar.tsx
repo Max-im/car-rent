11323b9b28012b6841b0891c54a043703795dbe8
max-im <pogidaevmo@gmail.com>
2024-05-24 00:12:37 +0300

    fix navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,26 @@
+'use client'
+
 import Link from "next/link"
 import Image from "next/image"
-import { CustomButton } from "./"
+import { CustomButton } from "."
 
 export default function Navbar() {
+
+  const onHandleClick = () => {
+
+  }
   return (
     <header className="w-full absolute z-10">
         <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
             <Link href="/" className="justify-center flex items-center">
                 <Image src="/logo.svg" alt="logo" width="118" height="18" className="object-contain"/>
             </Link>
-            <CustomButton 
+
+            <CustomButton
                 title="Sign in"
                 btnType="button"
                 containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+                handleClick={onHandleClick}
             />
         </nav>
     </header>
